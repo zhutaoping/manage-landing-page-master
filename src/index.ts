f001a9c41650553f1fd0300b3f370d62452b37ee
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-07-08 23:21:07 +0800

    modal done

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ form();
 slider();
 
 const checkbox = document.querySelector("[data-checkbox]") as HTMLInputElement;
-const headerModal = document.querySelector("[data-header__modal]");
-const headerMenu = document.querySelector("[data-header__menu]");
+const headerModal = document.querySelector("[data-header__modal]")!;
+const headerMenu = document.querySelector("[data-header__menu]")!;
 const menuItems = document.querySelectorAll("[data-menu-item]");
-const overlay = document.querySelector("[data-overlay]");
+const overlay = document.querySelector("[data-overlay]")!;
 
 function toggleModal() {
-	overlay!.classList.toggle("checked");
-	headerModal!.classList.toggle("checked");
-	headerMenu!.classList.toggle("checked");
+	overlay.classList.toggle("checked");
+	headerModal.classList.toggle("checked");
+	headerMenu.classList.toggle("checked");
 	menuItems.forEach((item) => {
 		item.classList.toggle("checked");
 	});
@@ -24,7 +24,7 @@ checkbox.addEventListener("change", () => {
 	toggleModal();
 });
 
-overlay!.addEventListener("click", () => {
+overlay.addEventListener("click", () => {
 	toggleModal();
 	checkbox.checked = false;
 });
