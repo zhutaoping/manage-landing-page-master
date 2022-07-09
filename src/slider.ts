7e077c8095af63159d9ea43066708d439d1b8b6e
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-07-09 22:19:15 +0800

    slider testing

--- src/slider.ts
+++ src/slider.ts
@@ -1,3 +1,24 @@
-function slider() {}
+function slider() {
+	const btns = document.querySelectorAll(
+		"[data-carousel-btn]"
+	) as NodeListOf<HTMLElement>;
+
+	btns.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			const offset = btn.dataset.carouselBtn === "next" ? 1 : -1;
+			const slides = btn
+				.closest("[data-carousel]")! // Up the DOM
+				.querySelector("[data-slides]"); // Down the DOM
+
+			const activeSlide = slides!.querySelector("[data-active]") as HTMLElement;
+			let newIndex = [...slides!.children].indexOf(activeSlide!) + offset;
+			if (newIndex < 0) newIndex = slides!.children.length - 1;
+			if (newIndex >= slides!.children.length) newIndex = 0;
+			//@ts-ignore
+			slides!.children[newIndex].dataset.active = true;
+			delete activeSlide.dataset.active;
+		});
+	});
+}
 
 export default slider;
